79d731dc0889f1b4eb3b49a46f044ac26e4f9b4a
mavisver <luz7282@gmail.com>
2022-09-28 13:27:58 -0300

    backend

--- ProyectoIntegrador_SantiagoLuz/FrontEnd/src/app/app.module.ts
+++ ProyectoIntegrador_SantiagoLuz/FrontEnd/src/app/app.module.ts
@@ -13,7 +13,11 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
+import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,12 +29,16 @@ import { ProyectoComponent } from './components/proyecto/proyecto.component';
     EduacionComponent,
     HysComponent,
     FooterComponent,
-    ProyectoComponent
+    ProyectoComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    FormsModule,
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
